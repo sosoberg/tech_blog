444cfce8334f9a58b88f8d79299348c1ab2c5e31
Sam Oberg <samueloberg@hotmail.com>
2021-05-21 15:26:14 -0700

    fixed signup

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,9 +2,10 @@ const router = require('express').Router();
 const { User } = require('../../models');
 
 router.post('/', async (req, res) => {
+  console.log(req.body)
   try {
     const userData = await User.create(req.body);
-
+  
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
@@ -12,6 +13,7 @@ router.post('/', async (req, res) => {
       res.status(200).json(userData);
     });
   } catch (err) {
+    console.log(err)
     res.status(400).json(err);
   }
 });
